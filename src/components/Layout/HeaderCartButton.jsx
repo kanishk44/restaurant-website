12f80f25ee2df376dcbc74ad0b4c3bdfff891914
kanishk44 <kanishk.shedsale@gmail.com>
2025-03-30 19:45:12 +0530

    Added restaurant summary

---
+++ src/components/Layout/HeaderCartButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className="cursor-pointer font-inherit border-none bg-[#4d1601] text-white py-3 px-12 flex justify-around items-center rounded-3xl font-bold hover:bg-[#2c0d00]">
+      <span>Your Cart</span>
+      <span className="bg-[#b94517] py-1 px-4 rounded-3xl ml-4 font-bold">
+        0
+      </span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/components/Layout/RestaurantSummary.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const RestaurantSummary = () => {
+  return (
+    <div className="text-center max-w-[45rem] w-[90%] relative bg-[#383838] text-white rounded-[14px] p-4 shadow-lg mx-auto -mt-40">
+      <h2 className="text-3xl font-bold mb-4">
+        Delicious Food, Delivered To You
+      </h2>
+      <p className="mb-4">
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experienced chefs!
+      </p>
+    </div>
+  );
+};
+
+export default RestaurantSummary;
